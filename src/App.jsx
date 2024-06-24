7aca598edeb5f3f85c81d283726d5f1e17f2506a
Vicky Dhote <dhoteji034@gmail.com>
2024-06-24 08:50:37 +0530

    mutli time

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,37 @@ import { useGSAP } from '@gsap/react';
 
 
 const App = () => {
-  const[cbe,Setcbe]=useState("");
+  // const[cbe,Setcbe]=useState("");
   const container =useRef(null);
  
-    const animation = gsap.to(container.current, { 
-      x: 700,
-      y: 300,
-      width:50,
-      height:80,
-      duration:5,
-      yoyo: true,
-      ease: "bounce.out",
-      backgroundColor: "green",
-      opacity:"1",
-      borderRadius:"10%",
-      repeatCount:2,
-    })
     
-  
-  const  changebox=()=>{
-    Setcbe(animation);
-    }
+    
+  const changebox = () => {
+    gsap.fromTo(container.current,
+      {
+        x: 0,
+        y: 0,
+        width: 100,
+        height: 100,
+        backgroundColor: "red",
+        opacity: 1,
+        borderRadius: "0%"
+      },
+      {
+        x: 700,
+        y: 300,
+        width: 50,
+        height: 80,
+        duration: 5,
+        yoyo: true,
+        ease: "bounce.out",
+        backgroundColor: "green",
+        opacity: 1,
+        borderRadius: "10%",
+        repeat: 0,
+      }
+    );
+  };
   return (
     <div id="main">
      <button onClick={changebox} >change</button>
